88b00412ad673d732a64569d7221a56420882aaf
chief-j <ivan.masley@hotmail.com>
2017-07-18 16:47:07 +0300

    Перші кроки

---
+++ main.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="ua">
+	<head>
+		<meta charset="utf-8">
+		<title>Гуртом — торрент-толока</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+		<meta name="title" content="Гуртом — торрент-толока">
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/semantic.min.js"></script>
+		<script src="js/scripts.js"></script>
+		<link rel="stylesheet" href="css/semantic.css">
+		<link rel="stylesheet" href="css/fontello.css">
+	</head>
+<body>
+	<main>
+		<div class="downlaod-head">
+			<div class="download-inner">
+				<div class="ui container">
+					<? include_once 'mainmenu.html'; ?>
+				</div>
+				<div class="ui container heade">
+					<div class="ui grid">
+
+						<div class="four wide column">
+							<div class="poster">
+								<div class="poster-inner">
+									<img src="images/amerikansk-bogi_550.jpg" alt="">
+								</div>
+							</div>
+						</div>
+						<div class="twelve wide column">
+							<div class="inform">
+								<div class="inform-inner">
+									<div class="breadcrumb">
+										<a href="" class="section">Толока</a>
+										<i class="right icon-right-open icon divider"></i>
+										<a href="" class="section">Відео</a>
+										<i class="right icon-right-open icon divider"></i>
+										<a href="" class="section">HD українською</a>
+										<i class="right icon-right-big icon divider"></i>
+										<a href="" class="active section">Серіали в HD</a>
+									</div>
+									<div class="post-name">
+										Американські боги / American Gods (2017) WEBRip 1080p
+									</div>
+									<div class="bt_attached">
+										<span title="Рейтинг">
+											7.7
+										</span>
+										<span>
+											<a href="" title="Оцінити">
+												<i class="icon-star-empty"></i>
+											</a>
+										</span>
+										<span>
+											<a href="" title="Завантажити torrent файл">
+												<i class="icon-download"></i>
+											</a>
+										</span>
+										<span>
+											<a href="" title="Magnet посилання">
+												<i class="icon-magnet"></i>
+											</a>
+										</span>
+									</div>
+									<div class="sharing">
+										<span class="share-btn">
+											<a href="" title="Якщо! Dropdown з соціальними мережами. Або без dropdown">
+												<i class="icon-share"></i>
+											</a>
+										</span>
+										<span class="share-facebook">
+											<a href="" title="">
+												<i class="icon-facebook"></i>
+											</a>
+										</span>
+										<span class="share-gplus">
+											<a href="" title="">
+												<i class="icon-gplus"></i>
+											</a>
+										</span>
+										<span class="share-twitter">
+											<a href="Поділитися">
+												<i class="icon-twitter"></i>
+											</a>
+										</span>
+										<span class="rss">
+											<a title="RSS"href="">
+												<i class="icon-rss-1"></i>
+											</a>
+										</span>
+
+										<span class="favorite">
+											<a title="Стежити" href="">
+												<i class="icon-heart"></i>
+											</a>
+										</span>
+										<span class="watched">
+											<a title="Переглянуте" href="">
+												<i class="icon-eye"></i>
+											</a>
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+				<div class="bottom">
+					<div class="ui container">
+						<div class="ui grid">
+							<div class="four wide column">1</div>
+							<div class="twelve wide column">
+								<div class="bt_statistic">
+									<div class="ui statistics">
+										<div class="grey statistic">
+											<div class="value">
+												100
+											</div>
+											<div class="label">
+												<i class="icon-floppy"></i>
+												Завантажень
+											</div>
+										</div>
+										<div class="red statistic">
+											<div class="value">
+												500
+											</div>
+											<div class="label">
+												<i class="icon-down-circled"></i>
+												Завантажують
+											</div>
+										</div>
+										<div class="green statistic">
+											<div class="value">
+												1
+											</div>
+											<div class="label">
+												<i class="icon-up-circled"></i>
+												Роздають
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="bt_check">
+									<span><i class="icon-ok-circle"></i>Перевірено</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="thanks-container">
+			<div class="ui container">
+				<div class="ui grid">
+					<div class="four wide column"></div>
+					<div class="twelve wide column">
+						<div class="ui labeled button" tabindex="0">
+							<div class="ui red button">
+								<i class="icon-thumbs-up-alt icon"></i> Дякую
+							</div>
+							<a class="ui basic red left pointing label">
+								1,048
+							</a>
+						</div>
+
+						<div class="ui green icon button">
+							<i class="icon-align-justify icon"></i>
+						</div>
+
+						<div class="tag-list">
+							<div class="tags">
+								<a class="ui tag label">2017</a>
+								<a class="ui tag label">драма</a>
+								<a class="ui red tag label">фентезі</a>
+								<a class="ui teal tag label">містика</a>                  
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="ui container">
+			<div class="download-content">
+				<article>
+					<? include_once 'article.html'; ?>
+				</article>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<script src="js/script.js"></script>
+		<link rel="stylesheet/less" type="text/css" href="less/download.less" />
+		<script type="text/javascript">
+			less = {
+				env: "development",
+				async: false,
+				fileAsync: false,
+				poll: 10000,
+				functions: {},
+				dumpLineNumbers: "comments",
+				relativeUrls: false,
+				rootpath: ""
+			};
+		</script>
+		<script src="js/less.min.js" type="text/javascript"></script>
+	</footer>
+</body>
+</html>
